45e611db737afa13955d1f5eaad1f4b5a1c95b07
Mrfrankgithub <155208141+Mrfrankgithub@users.noreply.github.com>
2025-07-09 03:15:56 +0200

    update

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,25 @@
+// import { defineConfig } from "vite"
+// import react from "@vitejs/plugin-react"
+// import path from "path"
+
+// export default defineConfig({
+//   plugins: [react()],
+//   resolve: {
+//     alias: {
+//       "@": path.resolve(__dirname, "./src"),
+//     },
+//   },
+// })
+
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     // React plugin is required for JSX transformation
     react(),
-    tailwindcss(),
   ],
   server: {
     // Listen on all network interfaces
